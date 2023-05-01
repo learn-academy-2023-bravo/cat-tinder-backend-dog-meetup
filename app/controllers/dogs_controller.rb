7e4b735f886520ac1761f4116460d86a39e82846
LEARN Academy <sdunlap102326@gmail.com>
2023-05-01 14:58:31 -0700

    Receiving 2 errors on the test

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,22 @@
 class DogsController < ApplicationController
+    def index
+        dogs = Dog.all
+        render json: dogs
+    end
+
+    def create
+        dog = Dog.create
+        render json: dogs
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def dog_params
+        params.require(:dog).permit(:name, :age, :enjoys, :image)
+    end
 end
+
--- spec/requests/dogs_spec.rb
+++ spec/requests/dogs_spec.rb
@@ -2,6 +2,41 @@ require 'rails_helper'
 
 RSpec.describe "Dogs", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "gets a list of dogs" do 
+      Dogs.create(
+        name: 'Karter',
+        age: 3,
+        enjoys: 'Laying on a heating pad, and using the bathroom in the house. Ya know, casual stuff.',
+        image: 'https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-08/Yorkie-Poo-Yorkipoo.jpg?itok=EsHlkqjB'
+      )
+      # make our request to the specific endpoints
+      get '/dogs'
+
+      # we want our response to be formatted in JSON
+      dog = JSON.parse(response.body)
+      expect(response).to have_http_status(200)
+      expect(dog.length).to eq 1
+    end
+  end
+
+  describe "POST /create" do
+    it "creates a dog" do
+    dog_params = {
+      dog: {
+        name: 'Karter',
+        age: 3,
+        enjoys: 'Laying on a heating pad, and using the bathroom in the house. Ya know, casual stuff.',
+        image: 'https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-08/Yorkie-Poo-Yorkipoo.jpg?itok=EsHlkqjB'
+      }
+    }
+    # make the request to the specific endpoints
+    post "/dogs", params: dog_params
+    expect(response).to have_http_status(200)
+    dog = Dog.first
+    expect(dog.name).to eq 'Karter'
+    expect(dog.age).to eq 3
+    expect(dog.enjoys).to eq 'Laying on a heating pad, and using the bathroom in the house. Ya know, casual stuff.'
+    expect(dog.image).to eq 'https://www.purina.co.uk/sites/default/files/styles/square_medium_440x440/public/2022-08/Yorkie-Poo-Yorkipoo.jpg?itok=EsHlkqjB'
+  end
   end
 end
